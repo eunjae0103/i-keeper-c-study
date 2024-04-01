cfe4718dd0fb4b48dbcf1d0b1f0610dcf35edfeb
eunjae0103 <164455662+eunjae0103@users.noreply.github.com>
2024-04-01 17:14:15 +0900

    Create 10039.c

---
+++ 02week/10039.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    int sum = 0;
+    for (int i = 0; i < 5; i++) {
+        scanf("%d", &a);
+
+        if (a < 40)
+            sum += 40;
+        else sum += a;
+    }
+    printf("%d", sum / 5);
+
+    return 0;
+}
